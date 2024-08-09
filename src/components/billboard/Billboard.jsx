3627de40c9ef204a0979bda4f4ffad76bb97254a
maurobrizio <briziomauro2003@gmail.com>
2024-08-09 19:21:17 -0300

    fix: fixing errors

--- src/components/billboard/Billboard.jsx
+++ src/components/billboard/Billboard.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { Movies } from "../data/Data";
 
 const Billboard = () => {
   const navigate = useNavigate();
@@ -8,28 +7,23 @@ const Billboard = () => {
     navigate(`/movie-screening`);
   };
 
-
-
   return (
     <div className="container m-10 p-4  flex">
-      {Movies.map((movie)=>{
-        <div key={movie.id} className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 ">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 ">
         <div
           onClick={navigateToMovieScreening}
           className="text-white rounded-lg overflow-hidden shadow-md relative cursor-pointer "
         >
           <img
-            src={movie.img}
-            alt={movie.title}
+            src="https://image.tmdb.org/t/p/w500/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg"
+            alt="Ad Astra"
             className="w-full object-cover"
           />
           <h3 className="text-lg font-semibold mb-2 absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 p-2 text-center">
-            {movie.title}
+            Ad Astra
           </h3>
         </div>
       </div>
-      })}
-      
     </div>
   );
 };
