90778ec7542eab5f3f87dda72e4a001c4469b92a
maurobrizio <briziomauro2003@gmail.com>
2024-08-09 19:24:59 -0300

    fix:fixing errors

--- src/components/billboard/Billboard.jsx
+++ src/components/billboard/Billboard.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { Movies } from "../data/Data";
 
 const Billboard = () => {
   const navigate = useNavigate();
@@ -9,22 +10,24 @@ const Billboard = () => {
   };
 
   return (
-    <div className="container m-10 p-4  flex">
-      {Movies.map((movie)=>{
-        <div key={movie.id} className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 ">
-        <div
-          onClick={navigateToMovieScreening}
-          className="text-white rounded-lg overflow-hidden shadow-md relative cursor-pointer "
-        >
-          <img
-            src={movie.img}
-            alt={movie.title}
-            className="w-full object-cover"
-          />
-          <h3 className="text-lg font-semibold mb-2 absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 p-2 text-center">
-            {movie.title}
-          </h3>
-        </div>
+    <div className="container m-10 p-4 flex">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {Movies.map((movie) => (
+          <div
+            key={movie.id}
+            onClick={() => navigateToMovieScreening(movie.id)} // Pasar el ID directamente
+            className="text-white rounded-lg overflow-hidden shadow-md relative cursor-pointer"
+          >
+            <img
+              src={movie.img}
+              alt={movie.title}
+              className="w-full object-cover"
+            />
+            <h3 className="text-lg font-semibold mb-2 absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 p-2 text-center">
+              {movie.title}
+            </h3>
+          </div>
+        ))}
       </div>
     </div>
   );
