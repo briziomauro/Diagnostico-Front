0d8e7435a076d7bcbcf67476904bdb6bf1e26bab
maurobrizio <briziomauro2003@gmail.com>
2024-08-13 18:29:44 -0300

    style: some changes in the style of new movie and edit

--- src/components/editMovie/EditMovie.jsx
+++ src/components/editMovie/EditMovie.jsx
@@ -13,7 +13,6 @@ const EditMovie = () => {
   const [origen, setOrigen] = useState(movie.origen);
 
   const handleSave = () => {
-    // Actualizar la película en la base de datos o estado
     movie.title = title;
     movie.description = description;
     movie.director = director;
@@ -25,49 +24,60 @@ const EditMovie = () => {
     }
   };
 
+  const navBack = () => {
+    navigate('/billboard');
+  };
+
+
   return (
-    <div className="container mx-auto mt-10 p-6 bg-gray-900 text-white rounded-xl shadow-lg">
-      <h1 className="text-4xl font-bold mb-6">Editar Película</h1>
+    <div className="w-[700px] mx-auto mt-10 p-6 bg-zinc-900 bg-opacity-55 text-white">
+      <i onClick={navBack} className="bi bi-arrow-left-short text-2xl cursor-pointer" />
+      <h1 className="flex justify-center text-4xl mb-6">EDITAR PELICULA</h1>
+
       <div className="mb-4">
-        <label className="block text-sm font-bold mb-2">Título</label>
+        <label className="block text-sm mb-1">Título</label>
         <input
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="w-full p-3 rounded-md bg-gray-800 text-white"
+          className="w-full p-2 bg-zinc-800 text-white"
         />
       </div>
+
       <div className="mb-4">
-        <label className="block text-sm font-bold mb-2">Descripción</label>
+        <label className="block text-sm mb-1">Descripción</label>
         <textarea
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          className="w-full p-3 rounded-md bg-gray-800 text-white"
+          className="w-full p-2 bg-zinc-800 text-white"
         />
       </div>
+
       <div className="mb-4">
-        <label className="block text-sm font-bold mb-2">Director</label>
+        <label className="block text-sm mb-1">Director</label>
         <input
           type="text"
           value={director}
           onChange={(e) => setDirector(e.target.value)}
-          className="w-full p-3 rounded-md bg-gray-800 text-white"
+          className="w-full p-2 bg-zinc-800 text-white"
         />
       </div>
+
       <div className="mb-6">
-        <label className="block text-sm font-bold mb-2">Origen</label>
+        <label className="block text-sm mb-1">Origen</label>
         <input
           type="text"
           value={origen}
           onChange={(e) => setOrigen(e.target.value)}
-          className="w-full p-3 rounded-md bg-gray-800 text-white"
+          className="w-full p-2 bg-zinc-800 text-white"
         />
       </div>
+
       <button
         onClick={handleSave}
-        className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-3 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out"
+        className="bg-zinc-900 hover:bg-zinc-800 text-white py-3 px-6 shadow-lg transition duration-300 ease-in-out"
       >
-        Guardar Cambios
+        GUARDAR
       </button>
     </div>
   );
