812265bd3244acca6fcb65b08cbcfc97d71b52a0
maurobrizio <briziomauro2003@gmail.com>
2024-08-15 17:28:37 -0300

    fix: code optimization

--- src/components/billboard/Billboard.jsx
+++ src/components/billboard/Billboard.jsx
@@ -1,25 +1,16 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { Movies } from "../data/Data";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { useState } from "react";
 import DeleteMovie from "../deleteMovie/DeleteMovie";
-
 
 const Billboard = () => {
   const [selectedMovieId, setSelectedMovieId] = useState(null);
   const navigate = useNavigate();
 
-  const navigateToMovieScreening = (movieId) => {
-    navigate(`/movie-screening/${movieId}`);
+  const handleNavigate = (path) => {
+    navigate(path);
   };
-
-  const navigateToEditMovie = (movieId) => {
-    navigate(`/admin/edit-movie/${movieId}`);
-  };
-
-  // const navigateToDeleteMovie = (movieId) => {
-  //   navigate(`/admin/delete-movie/${movieId}`);
-  // };
 
   const handleDeleteMovie = (movieId) => {
     setSelectedMovieId(movieId);
@@ -27,12 +18,6 @@ const Billboard = () => {
 
   const closeDeleteMovie = () => {
     setSelectedMovieId(null);
-  };
-
-
-
-  const navigateToAddMovie = () => {
-    navigate(`/admin/add-movie`);
   };
 
   return (
@@ -47,14 +32,14 @@ const Billboard = () => {
               src={movie.img}
               alt={movie.title}
               className="w-full object-cover"
-              onClick={() => navigateToMovieScreening(movie.id)}
+              onClick={() => handleNavigate(`/movie-screening/${movie.id}`)}
             />
             <h3 className="text-lg font-semibold mb-2 absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 p-2 text-center">
               {movie.title}
             </h3>
             <div className="absolute top-2 right-2 flex space-x-2">
               <button
-                onClick={() => navigateToEditMovie(movie.id)}
+                onClick={() => handleNavigate(`/admin/edit-movie/${movie.id}`)}
                 className="bg-white hover:bg-zinc-300 text-black py-1 px-2 text-sm"
               >
                 EDITAR
@@ -66,14 +51,16 @@ const Billboard = () => {
                 ELIMINAR
               </button>
             </div>
-
           </div>
         ))}
-        <div onClick={navigateToAddMovie} className="flex items-center justify-center bg-zinc-800 bg-opacity-40 text-white hover:bg-opacity-80 transition-all duration-300 h-[411px] w-[288px] cursor-pointer">
+        <div
+          onClick={() => handleNavigate(`/admin/add-movie`)}
+          className="flex items-center justify-center bg-zinc-800 bg-opacity-40 text-white hover:bg-opacity-80 transition-all duration-300 h-[411px] w-[288px] cursor-pointer"
+        >
           <i className="bi bi-plus-lg text-5xl" />
         </div>
       </div>
-      {selectedMovieId && <DeleteMovie movieId={selectedMovieId} onClose={closeDeleteMovie}/>}
+      {selectedMovieId && <DeleteMovie movieId={selectedMovieId} onClose={closeDeleteMovie} />}
     </div>
   );
 };
